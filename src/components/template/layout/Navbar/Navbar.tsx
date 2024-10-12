9e914c48e5d322269bf6fb8278b35bc64f8f66d0
Alireza Khodayi <89078767+Alireza-Khodayi@users.noreply.github.com>
2024-10-12 21:20:33 +0330

    some changes in file structure and cleaner code

--- src/components/template/layout/Navbar/Navbar.tsx
+++ src/components/template/layout/Navbar/Navbar.tsx
@@ -1,28 +1,13 @@
-import { NavLink } from 'react-router-dom';
-import { AppRoutes } from '@/core/utilities/app-routes';
+import { Logo } from './Logo';
+import { NavLinks } from './NavLinks';
 
 function Navbar() {
   return (
-    <header className='border-b border-primary border-opacity-30 bg-base-300 shadow-lg'>
-      <nav className='container mx-auto w-full'>
-        <ul className='flex gap-3 py-4'>
-          <li>
-            <NavLink className='nav-link' to={AppRoutes.home}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className='nav-link' to={AppRoutes.pricing}>
-              Pricing
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className='nav-link' to={AppRoutes.product}>
-              Product
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className='border-b border-primary border-opacity-30 bg-base-300 shadow-lg w-full'>
+      <div className='container mx-auto w-full flex items-center justify-between'>
+        <Logo />
+        <NavLinks />
+      </div>
     </header>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,11 @@
 import { Navbar } from '@/components/template/layout/Navbar';
+import { HomeHeroSection } from '@/components/pages/Home';
 
 function HomePage() {
   return (
     <>
       <Navbar />
-      <main className='h-[calc(100vh-8rem)] md:rounded-lg mt-6 container mx-auto w-full hero-section-background bg-cover bg-center py-10 px-20'>
-        <section className=' flex flex-col h-[85%] items-center justify-center gap-10 text-center'>
-          <h1 className='font-semibold text-3xl md:text-4xl lg:text-5xl xl:text-7xl'>
-            You travel the world.
-            <br />
-            WorldWise keeps track of your adventures.
-          </h1>
-          <h2 className='w-[90%] text-xl text-zinc-400'>
-            A world map that tracks your footsteps into every city you can think
-            of. Never forget your wonderful experiences, and show your friends
-            how you have wandered the world.
-          </h2>
-        </section>
-      </main>
+      <HomeHeroSection />
     </>
   );
 }
