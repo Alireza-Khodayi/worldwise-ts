79cea84c0cea087539e24f7bb2558a6c7a31ba18
Alireza Khodayi <89078767+Alireza-Khodayi@users.noreply.github.com>
2024-10-14 02:25:27 +0330

    cities ui done

--- src/core/utilities/Icons.tsx
+++ src/core/utilities/Icons.tsx
@@ -1,4 +1,5 @@
 import EmailIcon from '@/assets/icons/Mail.svg?react';
 import KeyIcon from '@/assets/icons/Key.svg?react';
+import XIcon from '@/assets/icons/X.svg?react';
 
-export { EmailIcon, KeyIcon };
+export { EmailIcon, KeyIcon, XIcon };
